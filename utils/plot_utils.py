7d4a0f5c612c7105c88002bd905811d192b2f6f5
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2019-08-05 10:03:50 +0800

    modified:   utils/plot_utils.py

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -1,3 +1,4 @@
+import re, os
 import matplotlib.pyplot as plt
 
 
@@ -28,3 +29,19 @@ class MatPlot(object):
       val = [float(x) for x in re_str.findall(logstr)]
       idx = range(len(val))
     return (idx, val)
+
+
+def parse_logfile(args, myargs):
+  config = getattr(myargs.config, args.command)
+  matplot = MatPlot()
+  fig, ax = matplot.get_fig_and_ax()
+  if len(config.logfiles) == 1:
+    logfiles = config.logfiles * len(config.re_strs)
+  for logfile, re_str in zip(logfiles, config.re_strs):
+    RE_STR = re.compile(re_str)
+    (idx, val) = matplot.parse_logfile_using_re(logfile=logfile, re_str=RE_STR)
+    ax.plot(idx, val, label=re_str)
+  ax.legend()
+  matplot.save_to_png(
+    fig, filepath=os.path.join(args.outdir, config.title + '.png'))
+  pass
