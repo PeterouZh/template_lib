80c986c7b6f79b6bc926662afb45838e594e3ea8
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2019-09-01 13:45:04 +0800

    add AverageMeter

--- utils/logging_utils.py
+++ utils/logging_utils.py
@@ -203,4 +203,20 @@ class TextLogger(object):
         f.write('%3d: %s\n' % (itr, kwargs[arg]))
 
 
+class AverageMeter(object):
+  """Computes and stores the average and current value"""
 
+  def __init__(self):
+    self.reset()
+
+  def reset(self):
+    self.val = 0
+    self.avg = 0
+    self.sum = 0
+    self.count = 0
+
+  def update(self, val, n=1):
+    self.val = val
+    self.sum += val * n
+    self.count += n
+    self.avg = self.sum / self.count
