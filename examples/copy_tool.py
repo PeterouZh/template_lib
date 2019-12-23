255d8c41a4ac0fa2cab2fd0d04e35ac35f73c5b9
Peterou <zhoupengcv@sjtu.edu.cn>
2019-12-23 16:00:10 +0800

    Create copy_tool.py

---
+++ examples/copy_tool.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+import argparse
+
+import moxing as mox
+
+parser = argparse.ArgumentParser(description='copy tool')
+parser.add_argument('-s', type=str)
+parser.add_argument('-d', type=str)
+parser.add_argument('-t', type=str, default="copy")
+cfg = parser.parse_args()
+
+def main():
+    print('=> Copy file(s) from %s to %s ...' % (cfg.s, cfg.d))
+    if cfg.t == "copytree":
+        mox.file.copy_parallel(cfg.s, cfg.d)
+    elif cfg.t == 'copy':
+        mox.file.copy(cfg.s, cfg.d)
+    elif cfg.t == 'copytree_nooverwrite':
+        if os.path.exists(cfg.d):
+            print('Skip copying, %s exist!' % cfg.d)
+            return
+        mox.file.copy_parallel(cfg.s, cfg.d)
+    elif cfg.t == 'copy_nooverwrite':
+        if os.path.exists(cfg.d):
+            print('Skip copying, %s exist!' % cfg.d)
+            return
+        mox.file.copy_parallel(cfg.s, cfg.d)
+    else:
+        assert 0
+    print('=> End copy file(s) from %s to %s ...' % (cfg.s, cfg.d))
+
+if __name__ == '__main__':
+    main()
