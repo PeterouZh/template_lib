34972ff9de06912d5565ed109c1276da5310930a
Peterou <zhoupengcv@sjtu.edu.cn>
2019-12-23 16:02:51 +0800

    Update test_bash.py

--- examples/test_bash.py
+++ examples/test_bash.py
@@ -40,6 +40,31 @@ def modelarts_record_bash_command(args, myargs, command=None):
     myargs.logger.info("Don't use modelarts!")
 
 
+def setup_log_obs_dir():
+  """
+
+  :return:  kwargs['log_number']
+  """
+  kwargs = {}
+  try:
+    import moxing as mox
+    # Remove log_obs dir
+    log_obs = os.environ['DLS_TRAIN_URL']
+    if mox.file.exists(log_obs):
+      mox.file.remove(log_obs, recursive=True)
+    mox.file.make_dirs(log_obs)
+    # Get log dir number
+    log_number = log_obs.strip('/').split('/')[-1]
+    if not log_number.isdigit():
+      print('DLS_TRAIN_URL does not end with digital, ignore.')
+    else:
+      kwargs['log_number'] = log_number
+      kwargs['add_number_to_configfile'] = True
+  except:
+    pass
+  return kwargs
+
+
 class TestingUnit(unittest.TestCase):
 
   def test_bash(self, *tmp):
@@ -48,6 +73,8 @@ class TestingUnit(unittest.TestCase):
         export CUDA_VISIBLE_DEVICES=0
         export PORT=6001
         export TIME_STR=1
+        export DLS_TRAIN_URL=/tmp/logs/1
+        export RESULTS_OBS=s3://bucket-xx/ZhouPeng/results
         export PYTHONPATH=../..
         python -c "import test_bash; \
           test_bash.TestingUnit().test_bash($PORT)"
@@ -60,14 +87,20 @@ class TestingUnit(unittest.TestCase):
       os.environ['PORT'] = '6001'
     if 'TIME_STR' not in os.environ:
       os.environ['TIME_STR'] = '0' if utils.is_debugging() else '1'
+    if 'DLS_TRAIN_URL' not in os.environ:
+      os.environ['DLS_TRAIN_URL'] = '/tmp/logs/1'
+    if 'RESULTS_OBS' not in os.environ:
+      os.environ['RESULTS_OBS'] = 's3://bucket-xx/ZhouPeng/results'
 
+    print('DLS_TRAIN_URL: %s'%os.environ['DLS_TRAIN_URL'])
+    print('RESULTS_OBS: %s'%os.environ['RESULTS_OBS'])
     # func name
     outdir = os.path.join('results', sys._getframe().f_code.co_name)
     myargs = argparse.Namespace()
 
     def build_args():
       argv_str = f"""
-            --config ../configs/virtual_terminal.yaml \
+            --config ../configs/virtual_terminal.yaml 
             --resume False --resume_path None
             --resume_root None
             """
@@ -81,36 +114,20 @@ class TestingUnit(unittest.TestCase):
       return args, argv_str
     args, argv_str = build_args()
 
-    kwargs = {}
-    try:
-      # Clean log_obs dir
-      import moxing as mox
-      assert os.environ['DLS_TRAIN_URL']
-      log_obs = os.environ['DLS_TRAIN_URL']
-      if mox.file.exists(log_obs):
-        mox.file.remove(log_obs, recursive=True)
-      mox.file.make_dirs(log_obs)
-      # Get log dir number
-      log_number = log_obs.strip('/').split('/')[-1]
-      assert log_number.isdigit()
-      kwargs['log_number'] = log_number
-    except:
-      pass
+    kwargs = setup_log_obs_dir()
     args.outdir = outdir
     args, myargs = utils.config.setup_args_and_myargs(
-      args=args, myargs=myargs, start_tb=False, add_number_to_configfile=True,
-      **kwargs)
+      args=args, myargs=myargs, start_tb=False, **kwargs)
+
 
     old_command = ''
-    myargs.logger.info('Begin loop.')
     # Create bash_command.sh
     bash_file = os.path.join(args.outdir, 'bash_command.sh')
-    with open(bash_file, 'w') as f:
-      pass
+    open(bash_file, 'w').close()
     cwd = os.getcwd()
     args.configfile_old = args.configfile + '.old'
-    shutil.copy(args.configfile, args.configfile_old)
     # copy outdir to outdir_obs, copy bash_file to outdir_obs
+    myargs.logger.info('outdir_obs: %s', args.outdir_obs)
     modelarts_utils.modelarts_sync_results(args, myargs, join=True)
     
     # disable moxing copy_parallel output
@@ -122,12 +139,17 @@ class TestingUnit(unittest.TestCase):
       try:
         try:
           import moxing as mox
+          time.sleep(0.8)
           # copy oudir_obs to outdir
-          time.sleep(1)
           mox.file.copy_parallel(args.outdir_obs, args.outdir)
         except:
-          os.rename(args.configfile_old, args.configfile)
+          if not os.path.exists(args.configfile):
+            os.rename(args.configfile_old, args.configfile)
+          if not os.path.exists(bash_file):
+            open(bash_file, 'w').close()
           pass
+
+        # parse command
         shutil.copy(bash_file, cwd)
         try:
           with open(args.configfile, 'rt') as handle:
@@ -138,7 +160,8 @@ class TestingUnit(unittest.TestCase):
           print('Parse config.yaml error!')
           command = None
           old_command = ''
-        os.rename(args.configfile, args.configfile_old)
+
+        # execute command
         if command != old_command:
           old_command = command
           if type(command) is list and command[0].startswith('bash'):
@@ -175,23 +198,35 @@ class TestingUnit(unittest.TestCase):
                     file=err_f, flush=True)
               print(e.returncode, file=err_f, flush=True)
             err_f.close()
+          myargs.logger.info('EE')
 
-            # os.system(command)
-          modelarts_utils.modelarts_sync_results(args, myargs, join=True)
-        if hasattr(args, 'outdir_obs'):
+        # sync outdir to outdir_obs
+        # del configfile in outdir
+        os.rename(args.configfile, args.configfile_old)
+        # del bash_file in outdir
+        os.remove(bash_file)
+        # copy jobs.txt from log_obs to outdir
+        try:
           log_obs = os.environ['DLS_TRAIN_URL']
           jobs_file_obs = os.path.join(log_obs, 'jobs.txt')
           jobs_file = os.path.join(args.outdir, 'jobs.txt')
           if mox.file.exists(jobs_file_obs):
             mox.file.copy(jobs_file_obs, jobs_file)
-          os.remove(bash_file)
-          mox.file.copy_parallel(args.outdir, args.outdir_obs)
+        except:
+          pass
 
-      except:
-        modelarts_utils.modelarts_record_jobs(
-          args, myargs, str_info='Exception!')
-        import traceback
-        myargs.logger.info(traceback.format_exc())
+        try:
+          mox.file.copy_parallel(args.outdir, args.outdir_obs)
+        except:
+          pass
+
+      except Exception as e:
+        if str(e) == 'server is not set correctly':
+          print(str(e))
+        else:
+          modelarts_utils.modelarts_record_jobs(args, myargs, str_info='Exception!')
+          import traceback
+          myargs.logger.info(traceback.format_exc())
         modelarts_utils.modelarts_sync_results(args, myargs, join=True)
 
   def test_resnet(self, bs=32, gpu='0,1,2,3,4,5,6,7'):
