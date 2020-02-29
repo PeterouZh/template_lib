3b91532ba5fdebcf6f939ae657b87b52d6d34ee9
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2020-02-29 14:40:54 +0800

    TextLogger

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 from .utils_func import is_debugging, get_prefix_abb, get_eval_attr, get_ddp_attr, print_number_params
 from . import config
 from . import args_parser
-
+from .logging_utils import TextLogger
 
 
