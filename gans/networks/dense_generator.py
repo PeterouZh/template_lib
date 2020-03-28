de367f1e899fc0291b917d0ea42230d90af9f7ba
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2020-03-28 17:05:12 +0800

    gen get fixed arc

--- gans/networks/dense_generator.py
+++ gans/networks/dense_generator.py
@@ -1,3 +1,6 @@
+import logging
+import os
+import numpy as np
 import json
 from easydict import EasyDict
 import functools
@@ -24,16 +27,25 @@ class DenseGenerator_v1(nn.Module):
     self.cfg_upsample                  = get_attr_kwargs(cfg, 'cfg_upsample', **kwargs)
     self.num_cells                     = get_attr_kwargs(cfg, 'num_cells', **kwargs)
     self.cfg_cell                      = get_attr_kwargs(cfg, 'cfg_cell', **kwargs)
-    self.layer_op_idx                  = get_attr_kwargs(cfg, 'layer_op_idx', default=None, **kwargs)
     self.cfg_ops                       = get_attr_kwargs(cfg, 'cfg_ops', **kwargs)
     self.cfg_out_bn                    = get_attr_kwargs(cfg, 'cfg_out_bn', **kwargs)
+    self.fixed_arc_file                = get_attr_kwargs(cfg, 'fixed_arc_file', default=None, **kwargs)
+    self.fixed_epoch                   = get_attr_kwargs(cfg, 'fixed_epoch', default=0, **kwargs)
+    self.layer_op_idx                  = get_attr_kwargs(cfg, 'layer_op_idx', default=None, **kwargs)
+
+    self.device = torch.device(f'cuda:{comm.get_rank()}')
 
     if self.layer_op_idx is not None:
+      # "[1 1 0 1 2 0 2 1 2 0 2 2 2 2 2 2 2 2 2 2 2 1 2 2 2 2 2 2 2 2]"
       self.layer_op_idx = json.loads(self.layer_op_idx.replace(' ', ', ').replace('][', '], [').strip())
       assert len(self.layer_op_idx) % self.num_cells == 0
       num_edges_of_cell = len(self.layer_op_idx) // self.num_cells
+    elif self.fixed_arc_file is not None:
+      sample_arc = self._get_arc_from_file(fixed_arc_file=self.fixed_arc_file, fixed_epoch=self.fixed_epoch, nrows=1)
+      self.layer_op_idx = sample_arc.reshape(-1)
+      assert len(self.layer_op_idx) % self.num_cells == 0
+      num_edges_of_cell = len(self.layer_op_idx) // self.num_cells
 
-    self.device = torch.device(f'cuda:{comm.get_rank()}')
 
     self.l1 = nn.Linear(self.dim_z, (self.bottom_width ** 2) * self.linear_ch)
     self.conv1 = nn.Conv2d(in_channels=self.linear_ch, out_channels=self.ch,
@@ -95,6 +107,24 @@ class DenseGenerator_v1(nn.Module):
     elif (isinstance(m, nn.BatchNorm2d)):
       nn.init.normal_(m.weight.data, 1.0, 0.02)
       nn.init.constant_(m.bias.data, 0.0)
+
+  def _get_arc_from_file(self, fixed_arc_file, fixed_epoch, nrows=1):
+    if os.path.isfile(fixed_arc_file):
+      print(f'Using fixed_arc_file: {fixed_arc_file}, \tfixed_epoch: {fixed_epoch}')
+      with open(fixed_arc_file) as f:
+        while True:
+          epoch_str = f.readline().strip(': \n')
+          sample_arc = []
+          for _ in range(nrows):
+            class_arc = f.readline().strip('[\n ]')
+            sample_arc.append(np.fromstring(class_arc, dtype=int, sep=' '))
+          if fixed_epoch == int(epoch_str):
+            break
+      sample_arc = np.array(sample_arc)
+    else:
+      raise NotImplemented
+    print('fixed arcs: \n%s' % sample_arc)
+    return sample_arc
 
 
 @GENERATOR_REGISTRY.register()
