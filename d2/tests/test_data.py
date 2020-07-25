a01e6f8bb1b0e94c441aef6dd09bd751c7c97646
gengcong <geco012@126.com>
2020-07-25 14:00:28 +0800

    Add lsun.

--- d2/tests/test_data.py
+++ d2/tests/test_data.py
@@ -340,3 +340,36 @@ class TestingBuildNasBench101(unittest.TestCase):
     metadata = MetadataCatalog.get(registed_name_list[idx])
 
     pass
+
+
+class TestingBuildLSUN(unittest.TestCase):
+
+  def test_bedroom_train(self):
+    """
+
+    """
+    if 'CUDA_VISIBLE_DEVICES' not in os.environ:
+      os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    if 'PORT' not in os.environ:
+      os.environ['PORT'] = '6006'
+    if 'TIME_STR' not in os.environ:
+      os.environ['TIME_STR'] = '0' if utils.is_debugging() else '0'
+    # func name
+    assert sys._getframe().f_code.co_name.startswith('test_')
+    command = sys._getframe().f_code.co_name[5:]
+    class_name = self.__class__.__name__[7:] \
+      if self.__class__.__name__.startswith('Testing') \
+      else self.__class__.__name__
+    outdir = f'results/{class_name}/{command}'
+    os.makedirs(outdir, exist_ok=True)
+
+    import matplotlib.pylab as plt
+    from template_lib.d2.data.build_lsun import data_path, registed_name_list, \
+      registed_func_list, kwargs_list
+    from detectron2.data import MetadataCatalog
+
+    idx = -1
+    dataset_dicts = registed_func_list[idx](name=registed_name_list[idx], data_path=data_path, **kwargs_list[idx])
+    metadata = MetadataCatalog.get(registed_name_list[idx])
+
+    pass
