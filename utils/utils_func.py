5e706695536c90251046017c5fddfab123d38ebf
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2020-07-28 11:05:22 +0800

    topk_error

--- utils/utils_func.py
+++ utils/utils_func.py
@@ -4,6 +4,7 @@ import re
 import logging
 from numpy import array_equal
 from torch.nn.parallel import DistributedDataParallel
+import torch
 import json
 
 
@@ -95,9 +96,28 @@ def top_accuracy(output, target, topk=(1,)):
   for k in topk:
     correct_k = correct[:k].view(-1).float().sum(0)
     res.append(correct_k.mul_(1.0 / batch_size))
-
   return res
 
+
+def topk_errors(preds, labels, ks):
+  """Computes the top-k error for each k.
+  top1_err, top5_err = self._topk_errors(preds, labels, [1, 5])
+  """
+  err_str = "Batch dim of predictions and labels must match"
+  assert preds.size(0) == labels.size(0), err_str
+  # Find the top max_k predictions for each sample
+  _top_max_k_vals, top_max_k_inds = torch.topk(
+    preds, max(ks), dim=1, largest=True, sorted=True
+  )
+  # (batch_size, max_k) -> (max_k, batch_size)
+  top_max_k_inds = top_max_k_inds.t()
+  # (batch_size, ) -> (max_k, batch_size)
+  rep_max_k_labels = labels.view(1, -1).expand_as(top_max_k_inds)
+  # (i, j) = 1 if top i-th prediction for the j-th sample is correct
+  top_max_k_correct = top_max_k_inds.eq(rep_max_k_labels)
+  # Compute the number of topk correct predictions for each k
+  topks_correct = [top_max_k_correct[:k, :].view(-1).float().sum() for k in ks]
+  return [(1.0 - x / preds.size(0)) * 100.0 for x in topks_correct]
 
 class AverageMeter():
   """ Computes and stores the average and current value """
