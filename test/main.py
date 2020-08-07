3881fb1e0f5e263a219502a157e38152b6749f57
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2020-08-07 14:31:38 +0800

    test_run_with_config

--- test/main.py
+++ test/main.py
@@ -75,7 +75,44 @@ class TestingUnit(unittest.TestCase):
     shutil.rmtree(outdir, ignore_errors=True)
     os.makedirs(outdir, exist_ok=True)
 
+  def test_run_with_config(self):
+    """
+    Usage:
 
+        export CUDA_VISIBLE_DEVICES=0,1
+        export PORT=6006
+        export TIME_STR=1
+        export PYTHONPATH=./submodule
+        python detectron2_exp/tests/run_detectron2.py \
+          --config ./detectron2_exp/configs/detectron2.yaml \
+          --command train_scratch_mask_rcnn_dense_R_50_FPN_3x_gn_2gpu \
+          --outdir results/Detectron2/train_scratch_mask_rcnn_dense_R_50_FPN_3x_gn_2gpu
+
+    :return:
+    """
+    if 'CUDA_VISIBLE_DEVICES' not in os.environ:
+      os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    if 'PORT' not in os.environ:
+      os.environ['PORT'] = '6006'
+    if 'TIME_STR' not in os.environ:
+      os.environ['TIME_STR'] = '0' if utils.is_debugging() else '1'
+    # func name
+    assert sys._getframe().f_code.co_name.startswith('test_')
+    command = sys._getframe().f_code.co_name[5:]
+    class_name = self.__class__.__name__[7:] \
+      if self.__class__.__name__.startswith('Testing') \
+      else self.__class__.__name__
+    outdir = f'results/{class_name}/{command}'
+
+    argv_str = f"""
+                --config domain_adaptive_faster_rcnn_pytorch_exp/configs/domain_faster_rcnn.yaml
+                --command {command}
+                --outdir {outdir}
+                """
+    from template_lib.test.main import run
+    args, myargs = run(argv_str)
+
+    pass
 
 
 def run(argv_str=None):
@@ -89,7 +126,8 @@ def run(argv_str=None):
   if hasattr(myargs.config, 'datasets'):
     prepare_dataset(myargs.config.datasets, cfg=myargs.config)
 
-  main(myargs)
+  # main(myargs)
+  return args1, myargs
 
 if __name__ == '__main__':
   run()
