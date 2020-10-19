fe6f7a212fe7ba4f8a14be6806d80d442c2fdadc
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2020-10-19 12:17:29 +0800

    md plotresults

--- utils/plot_results.py
+++ utils/plot_results.py
@@ -160,6 +160,8 @@ class TestingPlot(unittest.TestCase):
       os.environ['CUDA_VISIBLE_DEVICES'] = '3'
     if 'TIME_STR' not in os.environ:
       os.environ['TIME_STR'] = '0' if utils.is_debugging() else '0'
+    from template_lib.v2.config_cfgnode.argparser import \
+      (get_command_and_outdir, setup_outdir_and_yaml, get_append_cmd_str, start_cmd_run)
 
     command, outdir = get_command_and_outdir(self, func_name=sys._getframe().f_code.co_name, file=__file__)
     argv_str = f"""
