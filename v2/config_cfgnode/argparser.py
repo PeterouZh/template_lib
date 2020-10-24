31d875df4ea6b153967e456f82e79670781378d4
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2020-10-24 18:48:06 +0800

    print argv

--- v2/config_cfgnode/argparser.py
+++ v2/config_cfgnode/argparser.py
@@ -1,3 +1,4 @@
+import sys
 import argparse
 import logging
 import os
@@ -122,4 +123,5 @@ def update_parser_defaults_from_yaml(parser, name='args', use_cfg_as_args=False)
   parser_set_defaults(parser, cfg=default_args,
                       tl_imgdir=tl_imgdir, tl_ckptdir=tl_ckptdir, tl_textdir=tl_textdir,
                       tl_logfile=tl_logfile)
+  logging.getLogger('tl').info('sys.argv: \n python \n' + ' \ \n'.join(sys.argv))
   return parser
