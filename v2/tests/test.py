e75909d79846652c95d10f6faabe3b8939ad2080
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2020-12-24 14:05:35 +0800

    plot

--- v2/tests/test.py
+++ v2/tests/test.py
@@ -372,3 +372,73 @@ class Testing_v2_cfgnode(unittest.TestCase):
     fig.savefig(saved_file, bbox_inches='tight', pad_inches=0.01)
     print(f'Save to {saved_file}')
     pass
+
+  def test_save_OmniGAN_ImageNet128_IS(self):
+    """
+    Usage:
+        export TIME_STR=1
+        export PYTHONPATH=./exp:./BigGAN_PyTorch_1_lib:./
+        python -c "from exp.tests.test_BigGAN_v1 import Testing_Figures;\
+          Testing_Figures().test_save_early_collapse_on_cifar100()"
+
+    :return:
+    """
+    if 'CUDA_VISIBLE_DEVICES' not in os.environ:
+      os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    if 'TIME_STR' not in os.environ:
+      os.environ['TIME_STR'] = '0' if utils.is_debugging() else '0'
+    from template_lib.v2.config_cfgnode.argparser import get_command_and_outdir, setup_outdir_and_yaml, \
+      get_append_cmd_str, start_cmd_run
+    from template_lib.v2.matplot import set_font
+    set_font()
+
+    command, outdir = get_command_and_outdir(self, func_name=sys._getframe().f_code.co_name, file=__file__)
+    argv_str = f"""
+                    --tl_config_file exp/tests/configs/Figures.yaml
+                    --tl_command {command}
+                    --tl_outdir {outdir}
+                    """
+    args, cfg = setup_outdir_and_yaml(argv_str, return_cfg=True)
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import pickle
+    from template_lib.utils import colors_dict
+
+    fig, ax = plt.subplots()
+    # ax.set_xticks(range(0, 600, 100))
+    ax.tick_params(labelsize=cfg.fontsize.tick_fs)
+    ax.set_xlabel(cfg.xlabel, fontsize=cfg.fontsize.xylabel_fs)
+    ax.set_ylabel(cfg.ylabel, fontsize=cfg.fontsize.xylabel_fs)
+
+    properties = cfg.get('properties', {})
+    ax.set(**properties)
+    with open(cfg.data_pickle, 'rb') as f:
+      data_list = pickle.load(f)
+    load_data_dict = data_list[cfg.data_index]
+
+    for idx, (_, data_dict) in enumerate(cfg.lines.items()):
+      data = load_data_dict[data_dict.key]
+
+      if 'clip_x' in cfg:
+        data_xlim = cfg.clip_x[-1]
+        data = data[data[:, 0] <= data_xlim]
+
+      if cfg.get_min_value:
+        best_index = data[:, 1].argmin()
+      else:
+        best_index = data[:, 1].argmax()
+      best_x = int(data[:, 0][best_index])
+      best_y = data[:, 1][best_index]
+
+      if cfg.get('add_auxi_label', False):
+        data_dict.properties.label = f'x_{best_x}-y_{best_y:.3f}-' + getattr(data_dict.properties, 'label', '')
+      ax.plot(data[:, 0], data[:, 1], color=colors_dict[data_dict.color], **data_dict.properties)
+      pass
+
+    ax.legend(prop={'size': cfg.fontsize.legend_size}, ncol=1)
+    fig.show()
+    saved_file = os.path.join(args.tl_outdir, cfg.saved_file)
+    fig.savefig(saved_file, bbox_inches='tight', pad_inches=0.01)
+    print(f'Save to {saved_file}')
+    pass
