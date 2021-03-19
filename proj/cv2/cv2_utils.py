50a1230494e02dfe5f3156625a8fc734c7fca637
Zhou Peng <zhoupengcv@sjtu.edu.cn>
2021-03-19 19:04:02 +0800

    pil

---
+++ proj/cv2/cv2_utils.py
@@ -0,0 +1,97 @@
+import os
+import sys
+import unittest
+from PIL import Image, ImageDraw, ImageFont
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+
+from template_lib import utils
+
+
+class VideoWriter(object):
+  def __init__(self, outfile, w, h, fps):
+    out_size = (w, h)
+    self.video = cv2.VideoWriter(outfile, cv2.VideoWriter_fourcc(*'mp4v'), fps, out_size)
+    pass
+
+  def write(self, image, is_tensor=True, rgb=True):
+    if is_tensor:
+      from torchvision.transforms.functional import to_pil_image
+      image = to_pil_image(image)
+    image = np.array(image)
+    if rgb:
+      image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    self.video.write(image)
+    pass
+
+  def release(self):
+    self.video.release()
+    pass
+
+
+class Testing_cv2_utils(unittest.TestCase):
+
+  def test_zoom_in_video_writer(self, debug=True):
+    """
+    Usage:
+        export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+        export TIME_STR=1
+        export PYTHONPATH=./exp:./stylegan2-pytorch:./
+        python 	-c "from exp.tests.test_styleganv2 import Testing_stylegan2;\
+          Testing_stylegan2().test_train_ffhq_128()"
+
+    :return:
+    """
+    if 'CUDA_VISIBLE_DEVICES' not in os.environ:
+      os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    if 'TIME_STR' not in os.environ:
+      os.environ['TIME_STR'] = '0' if utils.is_debugging() else '0'
+    from template_lib.v2.config_cfgnode.argparser import \
+      (get_command_and_outdir, setup_outdir_and_yaml, get_append_cmd_str, start_cmd_run)
+
+    tl_opts = ' '.join(sys.argv[sys.argv.index('--tl_opts') + 1:]) if '--tl_opts' in sys.argv else ''
+    print(f'tl_opts:\n {tl_opts}')
+
+    command, outdir = get_command_and_outdir(self, func_name=sys._getframe().f_code.co_name, file=__file__)
+    argv_str = f"""
+                --tl_config_file none
+                --tl_command none
+                --tl_outdir {outdir}
+                --tl_opts {tl_opts}
+                """
+    args, cfg = setup_outdir_and_yaml(argv_str, return_cfg=True)
+
+    n_gpus = len(os.environ['CUDA_VISIBLE_DEVICES'].split(','))
+    import tqdm
+    from template_lib.proj.pil.pil_utils import get_size
+
+    img_path = "template_lib/datasets/images/zebra_GT_target_origin.png"
+    outvid = f"{args.tl_outdir}/test.mp4"
+
+    out_size = 2048
+    image = Image.open(img_path)
+    w, h = image.size
+    max_scale = out_size / min(w, h)
+    out_w, out_h = get_size(w=w, h=h, dst_size=out_size)
+
+    out_video = VideoWriter(outfile=outvid, w=out_w, h=out_h, fps=10)
+
+    for scale in tqdm.tqdm(np.arange(1, max_scale, 0.05)):
+      out_img = Image.new(mode='RGB', size=(out_w, out_h), color='black')
+      cur_w, cur_h = int(w * scale), int(h * scale)
+      cur_image = image.resize((cur_w, cur_h), resample=Image.NEAREST)
+      xy = (out_w - cur_w) // 2, (out_h - cur_h) // 2
+      out_img.paste(cur_image, xy)
+      out_video.write(out_img, is_tensor=False, rgb=True)
+    out_video.release()
+    pass
+
+
+
+
+
+
+
--- proj/pil/pil_utils.py
+++ proj/pil/pil_utils.py
@@ -12,6 +12,17 @@ from template_lib import utils
 
 _font = r'template_lib/datasets/sans-serif.ttf'
 
+def get_size(w, h, dst_size, for_min_edge=True):
+  if for_min_edge:
+    edge = min(w, h)
+  else:
+    edge = max(w, h)
+
+  w = int(dst_size / edge * w)
+  h = int(dst_size / edge * h)
+  return w, h
+
+
 def add_text_in_tensor(img_tensor,
                        text,
                        xy=(0, 0),
@@ -53,7 +64,7 @@ def sr_merge_original_image_and_patches(image,
   """
   image = copy.deepcopy(image)
   im_w, im_h = image.size
-  resized_w = im_w // 2 - pad
+  resized_w = im_w // 2 - pad // 2
   resized_h = int((resized_w / w) * h)
 
   out_size = (im_w, im_h + pad + resized_h)
@@ -73,6 +84,36 @@ def sr_merge_original_image_and_patches(image,
   out_image.paste(image, (0, 0))
   return out_image
 
+def sr_merge_original_image_and_patches_tensor(img_tensor,
+                                               lefts,
+                                               uppers,
+                                               w,
+                                               h,
+                                               pad=3,
+                                               width=2,
+                                               patch_width=1,
+                                               outlines=('#ff0000', '#0000ff')):
+  import torchvision
+  import torchvision.transforms.functional as tv_trans_f
+
+  unsqueeze = True if img_tensor.dim() == 4 else False
+
+  if img_tensor.dim() == 3:
+    img_tensor = img_tensor.unsqueeze(0)
+  assert img_tensor.dim() == 4 and img_tensor.size(0) == 1
+
+  img_tensor = torchvision.utils.make_grid(img_tensor.cpu(), nrow=1, padding=0, normalize=True)
+  img_pil = tv_trans_f.to_pil_image(img_tensor)
+
+  img_pil = sr_merge_original_image_and_patches(
+    image=img_pil, lefts=lefts, uppers=uppers, w=w, h=h, pad=pad, width=width, patch_width=patch_width,
+    outlines=outlines)
+
+  img_tensor = tv_trans_f.to_tensor(img_pil)
+  if unsqueeze:
+    img_tensor = img_tensor.unsqueeze(0)
+  return img_tensor
+
 
 def draw_rectangle_and_return_crop(image, left, upper, w, h, outline='#ff0000', width=2, patch_width=1):
   right = left + w
@@ -81,7 +122,8 @@ def draw_rectangle_and_return_crop(image, left, upper, w, h, outline='#ff0000', 
 
   patch = image.crop((left, upper, right, lower))
   draw_patch = ImageDraw.Draw(patch)
-  patch_bbox = ((0, 0, patch.size[0]-patch_width, patch.size[1]-patch_width))
+  # patch_bbox = ((0, 0, patch.size[0]-patch_width, patch.size[1]-patch_width))
+  patch_bbox = ((0, 0, patch.size[0] - 1, patch.size[1] - 1))
   draw_patch.rectangle(patch_bbox, outline=outline, width=patch_width)
 
   draw = ImageDraw.Draw(image)
@@ -237,6 +279,66 @@ class Testing_pil_utils(unittest.TestCase):
     out_image = sr_merge_original_image_and_patches(image=image, lefts=left, uppers=upper, w=w, h=h, pad=pad,
                                                     width=3, patch_width=1)
 
+    fig, axes = plt.subplots(1, 1)
+    axes.imshow(out_image)
+    # axes[1].imshow(patch)
+    fig.show()
+    pass
+
+  def test_sr_merge_original_image_and_patches_tensor(self, debug=True):
+    """
+    Usage:
+        export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+        export TIME_STR=1
+        export PYTHONPATH=./exp:./stylegan2-pytorch:./
+        python 	-c "from exp.tests.test_styleganv2 import Testing_stylegan2;\
+          Testing_stylegan2().test_train_ffhq_128()"
+
+    :return:
+    """
+    if 'CUDA_VISIBLE_DEVICES' not in os.environ:
+      os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    if 'TIME_STR' not in os.environ:
+      os.environ['TIME_STR'] = '0' if utils.is_debugging() else '0'
+    from template_lib.v2.config_cfgnode.argparser import \
+      (get_command_and_outdir, setup_outdir_and_yaml, get_append_cmd_str, start_cmd_run)
+
+    tl_opts = ' '.join(sys.argv[sys.argv.index('--tl_opts') + 1:]) if '--tl_opts' in sys.argv else ''
+    print(f'tl_opts:\n {tl_opts}')
+
+    command, outdir = get_command_and_outdir(self, func_name=sys._getframe().f_code.co_name, file=__file__)
+    argv_str = f"""
+                --tl_config_file none
+                --tl_command none
+                --tl_outdir {outdir}
+                --tl_opts {tl_opts}
+                """
+    args, cfg = setup_outdir_and_yaml(argv_str, return_cfg=True)
+
+    n_gpus = len(os.environ['CUDA_VISIBLE_DEVICES'].split(','))
+    import torchvision.transforms.functional as trans_f
+
+    img_path = "template_lib/datasets/images/zebra_GT_target_origin.png"
+    # img_path = "results/Recons_SR_web_OmniINRGAN256/load_G_recons_sr_OmniInrGAN256/imgdir/zebra_GT_reconstruction64x.png"
+
+    left = [640, 2300]
+    upper = [960, 800]
+    w = 400
+    h = 160
+    pad = 20
+    size = 2048
+    width = 20
+    patch_width = 5
+
+    image = Image.open(img_path)
+    img_tensor = trans_f.to_tensor(image)
+    img_tensor = trans_f.resize(img_tensor, size=size, interpolation=Image.NEAREST)
+
+    out_image = sr_merge_original_image_and_patches_tensor(
+      img_tensor=img_tensor, lefts=left, uppers=upper, w=w, h=h, pad=pad,
+      width=width, patch_width=patch_width)
+
+    out_image = trans_f.to_pil_image(out_image)
     fig, axes = plt.subplots(1, 1)
     axes.imshow(out_image)
     # axes[1].imshow(patch)
