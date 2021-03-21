c6598794dd8b957583a2411a819cff35766fd65b
Zhou Peng <zhoupengcv@sjtu.edu.cn>
2021-03-21 16:55:09 +0800

    stream it

--- proj/streamlit/utils.py
+++ proj/streamlit/utils.py
@@ -21,13 +21,13 @@ def parse_list_from_st_text_input(label, value):
 
 
 def read_image_list_and_show_in_st(image_list_file, columns=['path', 'class_id']):
-  all_image_list = read_image_list_from_files(image_list_file)
-
-  image_list_df = pd.DataFrame(all_image_list, columns=columns)
+  st.header("Image list file: ")
   header = ",\n ".join(image_list_file)
   st.write(header)
+
+  all_image_list = read_image_list_from_files(image_list_file)
+  image_list_df = pd.DataFrame(all_image_list, columns=columns)
   st.dataframe(image_list_df)
-  print(f"Loaded dataframe from {header}")
   return all_image_list
 
 
