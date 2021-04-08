355db74d167059c843b2205199eb2e61eb458441
Zhou Peng <zhoupengcv@sjtu.edu.cn>
2021-04-08 12:40:47 +0800

    data list

---
+++ proj/ubuntu/get_data_list.py
@@ -0,0 +1,32 @@
+import pprint
+import argparse
+import sys
+sys.path.insert(0, '.')
+
+from template_lib.utils import get_filelist_recursive
+from template_lib.v2.logger.logger import get_file_logger
+
+def main():
+
+  file_list = get_filelist_recursive(directory=args.source_dir, ext=args.ext, sort=True)
+
+  out_f = get_file_logger(args.outfile)
+  for path in file_list:
+    out_f.info_msg(path)
+
+  pass
+
+if __name__ == '__main__':
+  """
+  python3 template_lib/proj/ubuntu/get_data_list.py \
+    --source_dir  --outfile  --ext *.png
+  """
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--source_dir', type=str, default="")
+  parser.add_argument('--outfile', type=str, default="")
+  parser.add_argument('--ext', type=str, nargs='+', default=["*.png"])
+
+  args = parser.parse_args()
+  pprint.pprint(vars(args))
+  main()
+
