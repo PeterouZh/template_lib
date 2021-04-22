a56f6a4e6a87725f09d733b8faaf6b00e0997cc6
Zhou Peng <zhoupengcv@sjtu.edu.cn>
2021-04-22 14:33:00 +0800

    st dataframe

--- proj/streamlit/utils.py
+++ proj/streamlit/utils.py
@@ -64,6 +64,7 @@ def selectbox(label, options, index=0, sidebar=False):
   else:
     ret = st.selectbox(label=label, options=options, index=index)
   logging.getLogger('st').info(f"{label}={ret}")
+  st.write(f"{label}={ret}")
   print(f"{label}={ret}")
   return ret
 
@@ -122,7 +123,7 @@ def parse_list_from_st_text_input(label, value, sidebar=False):
   return parsed_value
 
 
-def read_image_list_and_show_in_st(image_list_file, columns=['path', 'class_id'], header=None):
+def read_image_list_and_show_in_st(image_list_file, columns=['path', 'class_id'], header=None, show_dataframe=True):
   if not isinstance(image_list_file, (list, tuple)):
     image_list_file = [image_list_file, ]
 
@@ -135,13 +136,15 @@ def read_image_list_and_show_in_st(image_list_file, columns=['path', 'class_id']
     st.write(image_file)
 
   all_image_list = read_image_list_from_files(image_list_file)
-  image_list_df = pd.DataFrame(all_image_list, columns=columns)
-  st.dataframe(image_list_df)
+  if show_dataframe:
+    image_list_df = pd.DataFrame(all_image_list, columns=columns)
+    st.dataframe(image_list_df)
   return all_image_list
 
 
 def parse_image_list(image_list_file, header, columns=['path', ]):
-  image_list = read_image_list_and_show_in_st(image_list_file=image_list_file, columns=columns, header=header)
+  image_list = read_image_list_and_show_in_st(image_list_file=image_list_file, columns=columns, header=header,
+                                              show_dataframe=False)
 
   default_index = st.number_input(f"selected index (0~{len(image_list) - 1})", value=0,
                                   min_value=0, max_value=len(image_list) - 1, key=header)
