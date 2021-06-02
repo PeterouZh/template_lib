c9ce9afbeb1f2bf60e2228114582b609e672ab71
Zhou Peng <zhoupengcv@sjtu.edu.cn>
2021-06-02 16:52:56 +0800

    data list

--- proj/ubuntu/get_data_list.py
+++ proj/ubuntu/get_data_list.py
@@ -2,19 +2,25 @@ import pprint
 import argparse
 import sys
 sys.path.insert(0, '.')
+import tempfile
 
 from template_lib.utils import get_filelist_recursive
 from template_lib.v2.logger.logger import get_file_logger
+
 
 def main():
 
   file_list = get_filelist_recursive(directory=args.source_dir, ext=args.ext, sort=True)
   print(f"number of items: {len(file_list)}")
 
-  out_f = get_file_logger(args.outfile)
+  if not args.outfile:
+    fd, path = tempfile.mkstemp()
+    args.outfile = path
+
+  out_f = get_file_logger(args.outfile, stream=True)
   for path in file_list:
     out_f.info_msg(path)
-
+  print(f"outfile: {args.outfile}")
   pass
 
 if __name__ == '__main__':
